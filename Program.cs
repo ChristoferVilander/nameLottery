ef3a84a7ce12e3442ad82ff21f2ef498131bb00c
Christofer Vilander <6317669+ChristoferVilander@users.noreply.github.com>
2022-12-03 22:17:34 +0100

    Uploaded Project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Julklappspelet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv antalet deltagare");
+
+            int participants = Convert.ToInt32(Console.ReadLine());
+
+            List<string> peopleList = new List<string> { };
+
+            Console.WriteLine("Skriv namnen");
+            int j = 0;
+            while (j < participants)
+            {
+                peopleList.Add(Console.ReadLine());
+                j++;
+            }
+
+            Random rnd = new Random();
+            var shuffled = peopleList.OrderBy(_ => rnd.Next()).ToList();
+
+            Console.WriteLine("Det blir följande uppdelning");
+            int i = 0;
+            while (i < peopleList.Count)
+            {
+                Console.Write(shuffled[i] + " gives to ");
+
+                i++;
+
+                if (i == peopleList.Count) { break; }
+                Console.WriteLine(shuffled[i]);
+
+            }
+
+            Console.Write(shuffled[0]);
+        }
+    }
+}
